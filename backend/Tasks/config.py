c940f6a5b9bee245ae7ecacc22cc219ea7712edb
Jun Sheng <jskoh.2023@scis.smu.edu.sg>
2025-09-25 09:10:17 +0800

    feat(DB): established con

--- backend/Tasks/config.py
+++ backend/Tasks/config.py
@@ -1,8 +1,17 @@
 import os
+from dotenv import load_dotenv
 
-# TODO: BOILERPLATE
+load_dotenv()
+
 class Config:
     FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", "http://localhost:3000")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///dev.db")
+
+    USER = os.getenv("user")
+    PASSWORD = os.getenv("password")
+    HOST = os.getenv("host")
+    PORT = os.getenv("port")
+    DBNAME = os.getenv("dbname")
+
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
     SQLALCHEMY_ECHO = os.getenv("SQLALCHEMY_ECHO", "false").lower() == "true"
     ENV = os.getenv("ENV", "dev")
