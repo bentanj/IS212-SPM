026f73d31102fbb9ad3b93f740e1a9d7bcadeb49
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-10-06 21:46:14 +0800

    feat (task) : change Priority to be number based

--- frontend/src/constants/DefaultFormData.ts
+++ frontend/src/constants/DefaultFormData.ts
@@ -1,6 +1,6 @@
 import IFormData from "@/types/IFormData";
 
-export const PriorityOptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', ''];
+export const PriorityOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ''];
 
 export const StatusOptions = ["To Do", "In Progress", "Completed", "Blocked", ''];
 
