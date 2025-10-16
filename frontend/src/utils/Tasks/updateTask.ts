d989dc1c6055bc02992827a73f221fd29f05ca1a
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-10-16 16:30:55 +0800

    feat : create updateTask wrapper to trigger PUT API endpoint

--- frontend/src/utils/Tasks/updateTask.ts
+++ frontend/src/utils/Tasks/updateTask.ts
@@ -1,5 +1,26 @@
-import { Task } from "@/mocks/staff/taskMockData";
+import { Task } from "@/types";
 
-export default function updateTask(updatedTask: Task) {
-    return null;
+const TASK_PORT = process.env.TASK_SERVICE_PORT || 8000;
+
+export default async function updateTask(updatedTask: Task) {
+    const taskId = updatedTask.taskId;
+
+    try {
+        const response = await fetch(`http://localhost:${TASK_PORT}/api/tasks/${taskId}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(updatedTask),
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.json();
+            throw new Error(`HTTP error! Status: ${response.status}.\n${errorMessage.error}`);
+        }
+
+        return await response.json();
+    }
+    catch (error) {
+        console.error("Error creating task:", error);
+        throw error;
+    }
 }
