bbf98ec97df1e994d31abfa43da78d26fe2cec7a
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-10-22 17:52:57 +0800

    fix (CI Build Error) : remove not in use file

--- frontend/src/constants/RecurringRule.ts
+++
@@ -1,9 +0,0 @@
-import { RRule } from 'rrule';
-
-export const EVENT_FREQUENCY = {
-    "One-off": "",
-    "Daily": RRule.DAILY,
-    "Weekly": RRule.WEEKLY,
-    "Monthly": RRule.MONTHLY,
-    "Yearly": RRule.YEARLY,
-}
