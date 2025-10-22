549f7e06c9737213cbbba0d7ab584dff24be0b5d
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-10-22 17:55:11 +0800

    fix (CI Build Error) : removed file. Not in use

--- frontend/src/types/TRecurringDateParams.ts
+++
@@ -1,8 +0,0 @@
-import { EVENT_FREQUENCY } from "@/constants/RecurringRule";
-
-export interface TRecurringDateParams {
-    startDate: Date;
-    endDate: Date;
-    frequency: keyof typeof EVENT_FREQUENCY;
-    frequencyInterval: number;
-};
