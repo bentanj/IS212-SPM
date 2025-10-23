27b9aff6fe5a4c61039317473ede85f6d4c56b16
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-10-23 19:02:51 +0800

    feat : create function autoReplicateAllSubtasks

--- frontend/src/utils/recurringTask.ts
+++ frontend/src/utils/recurringTask.ts
@@ -1,5 +1,6 @@
-import { APITaskParams, RecurrenceFrequency } from "@/types";
+import { APITaskParams, RecurrenceFrequency, Task } from "@/types";
 import dayjs from "dayjs";
+import { AlertColor } from "@mui/material";
 
 const RecurrenceFreqOptionsMap: Record<RecurrenceFrequency, dayjs.ManipulateType | null> = {
     "One-Off": null,
@@ -42,4 +43,78 @@ export function recurringTaskDate(task: Omit<APITaskParams, "taskId">): dayjs.Da
 
     // Otherwise, return the original due date
     return newStartDate;
+}
+
+import { getSubtasks } from "@/utils/Tasks/getTask";
+import createTask from "./Tasks/createTask";
+export async function autoReplicateAllSubtasks(
+    parentTask: APITaskParams,
+    setSnackBarContent: (message: string, severity: AlertColor) => void
+) {
+    const subtasks = await getSubtasks(String(parentTask.taskId));
+    let newSubtasksCreated = 0;
+    let errorMessages = [];
+    for (const subtask of subtasks) {
+        const newSubtaskData = replicateRecurringSubtaskData(parentTask, subtask);
+        if (typeof newSubtaskData !== "string") {
+            try {
+                const newSubtask = {
+                    ...newSubtaskData,
+                    assigned_users: newSubtaskData.assignedUsers.map(user => user.userId),
+                }
+                await createTask(newSubtask);
+                newSubtasksCreated++;
+            } catch (error) {
+                const errorMessage = `Error creating recurring subtask ${newSubtaskData.title}: ${error instanceof Error ? error.message : String(error)}`;
+                console.error(errorMessage);
+                errorMessages.push(errorMessage);
+            }
+        }
+        else {
+            errorMessages.push(newSubtaskData);
+        }
+    }
+
+    if (errorMessages.length === 0) {
+        setSnackBarContent(`${newSubtasksCreated} recurring subtasks have been created.`, "success");
+    }
+    else {
+        const combinedErrors = errorMessages.join(" | ");
+        setSnackBarContent(
+            `Only ${newSubtasksCreated} out of ${subtasks.length} recurring subtasks have been created. Errors: ${combinedErrors}`,
+            "warning");
+    }
+}
+
+export function replicateRecurringSubtaskData(
+    parentTask: APITaskParams,
+    subtask: Task,
+) {
+    const parentFreq = RecurrenceFreqOptionsMap[parentTask.recurrenceFrequency];
+    const parentInterval = parentTask.recurrenceInterval;
+
+    // If there is no recurrence interval or no unit (One-Off), we can't compute a recurring date
+    const newSubtaskStartDate = dayjs(subtask.startDate.replace(" ", "T")).add(parentInterval!, parentFreq!);
+    const newSubtaskDueDate = dayjs(subtask.dueDate.replace(" ", "T")).add(parentInterval!, parentFreq!);
+
+    // If the new start date is after the original due date, return null
+    if (newSubtaskStartDate.isAfter(dayjs(parentTask.dueDate.replace(" ", "T")))) {
+        return `Subtask ${subtask.title} start date exceeds parent task due date`;
+    }
+
+    // If the new due date is after the original due date, return null
+    if (newSubtaskDueDate.isAfter(dayjs(parentTask.dueDate.replace(" ", "T")))) {
+        return `Subtask ${subtask.title} due date exceeds parent task due date`;
+    }
+
+    const newSubtask = {
+        ...subtask,
+        taskId: 0,  // Passing value of 0 to database will auto-generate a new ID
+        startDate: newSubtaskStartDate.toISOString(),
+        dueDate: newSubtaskDueDate.toISOString(),
+        completedDate: null,
+        status: "To Do",
+    };
+
+    return newSubtask;
 }
