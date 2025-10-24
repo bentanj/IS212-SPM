cb8e2ebc291043ef97ee86708ff6fa96dcf3d2e0
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-10-24 23:34:20 +0800

    fix : amend import statement format
    replicateRecurringTaskData no longer default import

--- frontend/src/utils/_Tests/_recurringTask.ts/replicateRecurringTaskData.test.ts
+++ frontend/src/utils/_Tests/_recurringTask.ts/replicateRecurringTaskData.test.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import replicateRecurringTaskData from '../../recurringTask';
+import { replicateRecurringTaskData } from '../../recurringTask';
 import dayjs from 'dayjs';
 import { APITaskParams } from '@/types';
 
