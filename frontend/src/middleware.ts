341b14521a1af2757f8ba6649d2d192f78128bdd
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-10-30 01:16:03 +0800

    chore : add middleware logic to prevent access to application if not logged in

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,1 +1,25 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+    const { pathname } = req.nextUrl;
+
+    // Allow access to the sign in page without redirect
+    if (pathname === "/SignIn" || pathname === "/NoPermission"
+    ) {
+        return NextResponse.next();
+    }
+
+    // Redirect unauthenticated users to /SignIn
+    if (!req.auth) {
+        return NextResponse.redirect(new URL(`/SignIn`, req.url));
+    }
+
+    // Allow authenticated users
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: ["/((?!api|_next/).*)"],
+    debug: true,
+};
