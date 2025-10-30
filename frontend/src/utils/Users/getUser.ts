d6297e3678b7b49f39b434cc34e7408f0596cf67
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-10-30 23:43:35 +0800

    feat : create getAllUsers API handler function

--- frontend/src/utils/Users/getUser.ts
+++ frontend/src/utils/Users/getUser.ts
@@ -2,6 +2,28 @@ import { User } from "@/types";
 
 const TASK_PORT = process.env.TASK_SERVICE_PORT || 8000;
 
+export async function getAllUsers(): Promise<User[]> {
+    let targetURL = `http://localhost:${TASK_PORT}/api/users/`;
+
+    try {
+        const response = await fetch(targetURL, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.json();
+            throw new Error(`HTTP error! Status: ${response.status}.\n${errorMessage.error}`);
+        }
+
+        return await response.json();
+    }
+    catch (error) {
+        console.error("Error getting all users:", error);
+        throw error;
+    }
+}
+
 export async function getUserByEmail(email: string): Promise<User> {
     email = encodeURIComponent(email.toLowerCase())
 
@@ -21,7 +43,7 @@ export async function getUserByEmail(email: string): Promise<User> {
         return await response.json();
     }
     catch (error) {
-        console.error("Error getting task by ID:", error);
+        console.error("Error getting user by email:", error);
         throw error;
     }
 }
