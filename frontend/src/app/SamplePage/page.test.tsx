c596793550d110776f3cc4d3aef7191c469e0e09
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-11-07 14:00:48 +0800

    chore : cleanup unused files

--- frontend/src/app/SamplePage/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import Hello from './page';
-
-test('renders the correct text', () => {
-    render(<Hello name="Mike" />);
-    expect(screen.getByText('Hello Mike')).toBeInTheDocument();
-});
